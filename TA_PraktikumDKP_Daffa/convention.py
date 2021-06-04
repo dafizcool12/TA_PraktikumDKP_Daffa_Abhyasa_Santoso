48005ce63b71ef064a982a7624a3496be7f37ee0
dafizcool12 <dafizcool12@gmail.com>
2021-06-05 02:24:14 +0700

    Class Uploaded

---
+++ TA_PraktikumDKP_Daffa/convention.py
@@ -0,0 +1,9 @@
+class Convention:
+    def __init__(self): 
+        self.unit1 = ""
+
+    def setUnit(self,unit1):      
+        self.unit1=unit1
+    def getUnit(self):
+        return self.unit1
+    
